3780db8a2c56f943df11ea4be2ee5e632aa62591
GodzillaOO7 <abianandh007@gmail.com>
2023-06-19 08:40:54 +0000

    full project

---
+++ dotnetapp/AC_SERVICE_API/Database/AC_ServerDbContext.cs
@@ -0,0 +1,21 @@
+using AC_Service_API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+namespace AC_Service_API.Database
+{
+    public class AC_ServerDbContext:DbContext
+    {
+        public AC_ServerDbContext(DbContextOptions<AC_ServerDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<ProductModel> Products { get; set; }
+        public DbSet<UserModel> Users { get; set; } 
+        public DbSet<ServiceCenterModel> Services { get; set; }
+
+        public DbSet<AdminModel> Admins { get; set; }
+        public DbSet<LoginModel> LoginModels { get; set; }
+    }
+}
