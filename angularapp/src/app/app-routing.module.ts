63a6f48d65039656adfb8e2159c6e2a4635d6b0b
rk10320 <rohitkumar.singh148@gmail.com>
2023-06-24 04:42:50 +0000

    Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,52 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { UserHomepageComponent } from './user/user-homepage/user-homepage.component';
-import { UserDashboardComponent } from './user/user-dashboard/user-dashboard.component';
-import { UserAppointmentComponent } from './user/user-appointment/user-appointment.component';
-import { AdminAddcenterComponent } from './admin/admin-addcenter/admin-addcenter.component';
-import { AdminCenterprofileComponent } from './admin/admin-centerprofile/admin-centerprofile.component';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full',
-  },
-  {
-    path: 'signup',
-    component: SignupComponent,
-  },
-  {
-    path: 'userhome',
-    component: UserHomepageComponent,
-  },
-  {
-    path: 'userdashboard',
-    component: UserDashboardComponent,
-  },
-  {
-    path: 'appointment',
-    component: UserAppointmentComponent,
-  },
-  {
-    path: 'adminaddcenter',
-    component: AdminAddcenterComponent,
-  },
-  {
-    path: 'admincenterprofile',
-    component: AdminCenterprofileComponent,
-  }
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -2,29 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { AdminAddcenterComponent } from './admin/admin-addcenter/admin-addcenter.component';
-import { AdminCenterprofileComponent } from './admin/admin-centerprofile/admin-centerprofile.component';
-import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent,
-    AdminAddcenterComponent,
-    AdminCenterprofileComponent,
-    AdminHeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
