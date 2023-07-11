43c84e96da1605b0c5997bc90e016c1c0ed78d32
rk10320 <rohitkumar.singh148@gmail.com>
2023-07-11 09:05:46 +0000

    Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
-
 import { LoginComponent } from './component/login/login.component';
 import { SignupComponent } from './component/signup/signup.component';
 import { AdminComponent } from './component/admin/admin.component';
@@ -14,22 +12,20 @@ import { DashboardComponent } from './component/user/dashboard/dashboard.compone
 import { AppointmentComponent } from './component/user/appointment/appointment.component';
 
 const routes: Routes = [
-   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'admin', component: AdminComponent },
-  { path: 'admin/addServiceCenter', component: AddServiceCenterComponent },
-  { path: 'admin/editServiceCenter', component: EditServiceCenterComponent },
-  { path: 'user', component: UserComponent },
-  { path: 'user/homepage', component: HomepageComponent },
-  { path: 'user/dashboard', component: DashboardComponent },
-  { path: 'user/appointment', component: AppointmentComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+ { path: 'login', component: LoginComponent },
+ { path: 'signup', component: SignupComponent },
+ { path: 'admin', component: AdminComponent },
+ { path: 'admin/addServiceCenter', component: AddServiceCenterComponent },
+ { path: 'admin/editServiceCenter', component: EditServiceCenterComponent },
+ { path: 'user', component: UserComponent },
+ { path: 'user/homepage', component: HomepageComponent },
+ { path: 'user/dashboard', component: DashboardComponent },
+ { path: 'user/appointment', component: AppointmentComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-
-  exports: [RouterModule],
+ imports: [RouterModule.forRoot(routes)],
+ exports: [RouterModule]
 })
-export class AppRoutingModule {}
-
+export class AppRoutingModule { }
