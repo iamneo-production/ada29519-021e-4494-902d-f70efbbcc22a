f148ef37ccc577824c4134f10dd571a83faf7385
DivyaKiran2001 <divyakirantatikonda@gmail.com>
2023-07-18 16:47:23 +0000

    front end

---
+++ dotnetapp/acservice/Models/UserModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace acservice.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string userRole { get; set; }
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public string confirmpassword { get; set; }
+        public string mobileNumber { get; set; }
+    }
+}
