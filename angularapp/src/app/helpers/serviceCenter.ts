49008fed144994f2811ff40f5d5de48cdb6758b7
rk10320 <rohitkumar.singh148@gmail.com>
2023-07-21 08:53:48 +0000

    Initial Comit

--- angularapp/src/app/helpers/serviceCenter.ts
+++
@@ -1,9 +0,0 @@
-export interface serviceCenter{
-    serviceCenterID:string;
-    serviceCenterName:string
-    serviceCenterPhone:string
-    serviceCenterAddress:string
-    serviceCenterImageUrl:string
-    serviceCenteramailId:string
-    serviceCenterDescription:string
-}
