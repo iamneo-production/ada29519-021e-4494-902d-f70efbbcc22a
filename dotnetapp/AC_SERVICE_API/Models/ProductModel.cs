75738f65dc3513a51c2632b2c4b880a4e2d63df3
rk10320 <rohitkumar.singh148@gmail.com>
2023-07-21 08:15:45 +0000

    commit

---
+++ dotnetapp/AC_SERVICE_API/Models/ProductModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AC_Service_API.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        
+        public string productName { get; set; }
+        public string productModelNo { get; set; }
+        
+        public string dateOfPurchase { get; set;}
+        public string contactNumber { get; set;}
+        public string problemDescription { get; set; }
+        
+        public string time { get; set; }
+        public string date{ get; set;}
+
+        public string maildid { get; set; }
+        public string servicecenter { get; set; }
+
+    }
+}
