e273febedd8d3fc1e8fc1eb71dc301956e072f40
rk10320 <rohitkumar.singh148@gmail.com>
2023-07-24 11:03:38 +0000

    Commit

--- angularapp/src/app/services/account.service.spec.ts
+++ angularapp/src/app/services/account.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AppointmentService } from './appointment.service';
+import { AccountService } from './account.service';
 
-describe('AppointmentService', () => {
-  let service: AppointmentService;
+describe('AccountService', () => {
+  let service: AccountService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AppointmentService);
+    service = TestBed.inject(AccountService);
   });
 
   it('should be created', () => {
