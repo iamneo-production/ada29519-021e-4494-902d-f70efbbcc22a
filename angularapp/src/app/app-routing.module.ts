c550a1f87b49fab4dabed2fc4e28da1cd249a429
DivyaKiran2001 <divyakirantatikonda@gmail.com>
2023-07-25 13:28:07 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,33 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './component/login/login.component';
-import { SignupComponent } from './component/signup/signup.component';
-import { AdmindashboardComponent } from './component/admin/admindashboard/admindashboard.component';
 import { AddServiceCenterComponent } from './component/admin/add-service-center/add-service-center.component';
-import { EditServiceCenterComponent } from './component/admin/edit-service-center/edit-service-center.component';
-import { DashboardComponent } from './component/user/dashboard/dashboard.component';
-import { HomepageComponent } from './component/user/homepage/homepage.component';
 import { AppointmentComponent } from './component/user/appointment/appointment.component';
-import { AdminGuard } from './guard/admin.guard';
-import { UserGuard } from './guard/user.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  // { path: 'admin/dashboard', component: AdmindashboardComponent },
-  // { path: 'admin/addServiceCenter', component: AddServiceCenterComponent },
-  // { path: 'admin/editServiceCenter', component: EditServiceCenterComponent },
-  // { path: 'user/homepage', component: HomepageComponent },
-  // { path: 'user/dashboard', component: DashboardComponent },
-  // { path: 'user/appointment', component: AppointmentComponent },
-
-  { path: 'admin/dashboard', component: AdmindashboardComponent ,canActivate:[AdminGuard]},
-  { path: 'admin/addServiceCenter', component: AddServiceCenterComponent,canActivate:[AdminGuard] },
-  { path: 'admin/editServiceCenter', component: EditServiceCenterComponent,canActivate:[AdminGuard] },
-  { path: 'user/homepage', component: HomepageComponent,canActivate:[UserGuard] },
-  { path: 'user/dashboard', component: DashboardComponent ,canActivate:[UserGuard]},
-  { path: 'user/appointment', component: AppointmentComponent ,canActivate:[UserGuard]},
+  { path: 'admin/addServiceCenter', component: AddServiceCenterComponent },
+  { path: 'user/appointment', component: AppointmentComponent},
  
 ];
 
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,44 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './component/login/login.component';
-import { SignupComponent } from './component/signup/signup.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ShareService } from './services/share.service';
 import { AuthService } from './services/auth.service';
 import { AppointmentService } from './services/appointment.service';
 import { ServicecenterService } from './services/servicecenter.service';
-import { CarouselComponent } from './component/carousel/carousel.component';
 import { AdminComponent } from './component/admin/admin.component';
 import { AddServiceCenterComponent } from './component/admin/add-service-center/add-service-center.component';
-import { EditServiceCenterComponent } from './component/admin/edit-service-center/edit-service-center.component';
-import { AdmindashboardComponent } from './component/admin/admindashboard/admindashboard.component';
 import { UserComponent } from './component/user/user.component';
 import { AppointmentComponent } from './component/user/appointment/appointment.component';
-import { DashboardComponent } from './component/user/dashboard/dashboard.component';
-import { HomepageComponent } from './component/user/homepage/homepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent,
-    AdminComponent,
     AddServiceCenterComponent,
-    EditServiceCenterComponent,
+    AdminComponent,
     UserComponent,
-    DashboardComponent,
-    AppointmentComponent,
-    HomepageComponent,
-    CarouselComponent,
-    AdmindashboardComponent
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
