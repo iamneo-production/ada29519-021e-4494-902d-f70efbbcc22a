9e58226334c4ee701bfa849e4e5d6e9055805c50
Jeevitha-51 <jeevithak21802@gmail.com>
2023-07-25 05:44:12 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,21 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './component/login/login.component';
-import { SignupComponent } from './component/signup/signup.component';
+// import { LoginComponent } from './component/login/login.component';
+// import { SignupComponent } from './component/signup/signup.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminComponent } from './component/admin/admin.component';
 import { AddServiceCenterComponent } from './component/admin/add-service-center/add-service-center.component';
 import { EditServiceCenterComponent } from './component/admin/edit-service-center/edit-service-center.component';
-import { UserComponent } from './component/user/user.component';
-import { HomepageComponent } from './component/user/homepage/homepage.component';
-import { DashboardComponent } from './component/user/dashboard/dashboard.component';
-import { AppointmentComponent } from './component/user/appointment/appointment.component';
+// import { UserComponent } from './component/user/user.component';
+// import { HomepageComponent } from './component/user/homepage/homepage.component';
+// import { DashboardComponent } from './component/user/dashboard/dashboard.component';
+// import { AppointmentComponent } from './component/user/appointment/appointment.component';
 import { ShareService } from './services/share.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+// import { CarouselComponent } from './component/carousel/carousel.component';
+import { AdmindashboardComponent } from './component/admin/admindashboard/admindashboard.component';
+
+
 
 
 
@@ -28,15 +32,17 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent,
+    // LoginComponent,
+    // SignupComponent,
     AdminComponent,
     AddServiceCenterComponent,
     EditServiceCenterComponent,
-    UserComponent,
+    // UserComponent,
     DashboardComponent,
-    AppointmentComponent,
-    HomepageComponent
+    // AppointmentComponent,
+    // HomepageComponent,
+    // CarouselComponent,
+    AdmindashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +51,7 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    Ng2SearchPipeModule   
+    Ng2SearchPipeModule 
   ],
   providers: [ShareService],
   bootstrap: [AppComponent]
