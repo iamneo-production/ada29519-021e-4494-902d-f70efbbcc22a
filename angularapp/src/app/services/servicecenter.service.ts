47b53b2d935100186ed5cdab6d33f8b8c086b8ac
ramananvenkatesan <ramananrvenkatesan@gmail.com>
2023-07-26 10:58:55 +0000

    final commit FE

--- angularapp/src/app/services/servicecenter.service.ts
+++ angularapp/src/app/services/servicecenter.service.ts
@@ -8,24 +8,23 @@ import { Observable } from 'rxjs';
 })
 export class ServicecenterService {
 
-  constructor(private http: HttpClient) { }
-  editserviceCenterURL = "https://8080-edbedcefdafadfffbabacbdeefceabbedabdfbab.project.examly.io/admin/editServiceCenter";
-  deleteserviceCenterURL = "https://8080-edbedcefdafadfffbabacbdeefceabbedabdfbab.project.examly.io/admin/deleteServiceCenter";
-  getserviceCenterURL = "https://8080-edbedcefdafadfffbabacbdeefceabbedabdfbab.project.examly.io/admin/getservicecenter";
-  imageserviceCenterURL = "https://8080-edbedcefdafadfffbabacbdeefceabbedabdfbab.project.examly.io/admin/image"
-
-  getService(): Observable<serviceCenter[]> {
+  constructor(private http:HttpClient ) { }
+  editserviceCenterURL="https://8080-bfdadcacfdfbdefffbabacbdeedecebedadbdbbef.project.examly.io/admin/editServiceCenter";
+  deleteserviceCenterURL="https://8080-bfdadcacfdfbdefffbabacbdeedecebedadbdbbef.project.examly.io/admin/deleteServiceCenter";
+  getserviceCenterURL="https://8080-bfdadcacfdfbdefffbabacbdeedecebedadbdbbef.project.examly.io/admin/getservicecenter"
+  serviceCenterURL="https://8080-bfdadcacfdfbdefffbabacbdeedecebedadbdbbef.project.examly.io/admin"
+                        
+  getService():Observable<serviceCenter[]>{
     return this.http.get<serviceCenter[]>(this.getserviceCenterURL)
 
+    } 
+  updateservice(ser:serviceCenter):Observable<serviceCenter>{
+    return this.http.put<serviceCenter>(this.editserviceCenterURL+'/'+ser.serviceCenterID,ser)
   }
-  updateservice(ser: serviceCenter): Observable<serviceCenter> {
-    return this.http.put<serviceCenter>(this.editserviceCenterURL + '/' + ser.serviceCenterID, ser)
+  deleteservice(id:string):Observable<serviceCenter>{
+    return this.http.delete<serviceCenter>(this.deleteserviceCenterURL+'/'+id)
   }
-  deleteservice(id: string): Observable<serviceCenter> {
-    return this.http.delete<serviceCenter>(this.deleteserviceCenterURL + '/' + id)
+  getimage(id:string):Observable<serviceCenter>{
+    return this.http.get<serviceCenter>(this.serviceCenterURL+'/'+id)
   }
-  getimage(id: string): Observable<serviceCenter> {
-    return this.http.get<serviceCenter>(this.imageserviceCenterURL + '/' + id)
   }
-}
-
