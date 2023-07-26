924f8ecb8096a1d3f046d967ad6ddd0185ea7c14
ramananvenkatesan <ramananrvenkatesan@gmail.com>
2023-07-26 11:09:29 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { LoginComponent } from './component/login/login.component';
 // import { SignupComponent } from './component/signup/signup.component';
-import { AdminComponent } from './component/admin/admin.component';
+// import { AdminComponent } from './component/admin/admin.component';
 // import { AddServiceCenterComponent } from './component/admin/add-service-center/add-service-center.component';
 // import { EditServiceCenterComponent } from './component/admin/edit-service-center/edit-service-center.component';
-import { UserComponent } from './component/user/user.component';
+// import { UserComponent } from './component/user/user.component';
 // import { HomepageComponent } from './component/user/homepage/homepage.component';
 import { DashboardComponent } from './component/user/dashboard/dashboard.component';
 // import { AppointmentComponent } from './component/user/appointment/appointment.component';
@@ -17,11 +17,11 @@ const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   // { path: 'login', component: LoginComponent },
   // { path: 'signup', component: SignupComponent },
-  // { path: 'admin/dashboard', component: AdmindashboardComponent },
+  { path: 'admin/dashboard', component: AdmindashboardComponent },
   // { path: 'admin/addServiceCenter', component: AddServiceCenterComponent },
   // { path: 'admin/editServiceCenter', component: EditServiceCenterComponent },
   // { path: 'user/homepage', component: HomepageComponent },
-  // { path: 'user/dashboard', component: DashboardComponent },
+  { path: 'user/dashboard', component: DashboardComponent },
   // { path: 'user/appointment', component: AppointmentComponent },
 
   // { path: 'admin/dashboard', component: AdmindashboardComponent ,canActivate:[AdminGuard]},
