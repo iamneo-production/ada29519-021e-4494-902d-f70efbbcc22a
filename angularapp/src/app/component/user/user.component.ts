356ac92e1817af21396f325dd37ad1432d37b303
Aravindan-J7 <jaravinthan3@gmail.com>
2023-07-29 09:36:53 +0000

    final commit

---
+++ angularapp/src/app/component/user/user.component.ts
@@ -0,0 +1,46 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Account } from 'src/app/helpers/account';
+
+import ValidateForm from 'src/app/helpers/validateForm';
+import { AccountService } from 'src/app/services/account.service';;
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit{
+  userarr:Account[]=[]
+  isDropdownOpen = false;
+  email=localStorage.getItem('Email')||""
+  errormessage=''
+  successmessage=''
+  isNavbarTogglerActive = false;
+  UserData:any
+  constructor(private acc:AccountService,private router:Router,private fb:FormBuilder) { }
+  ngOnInit(): void {
+    this.getaccount(this.email)
+  }
+  getaccount(email:string){
+    this.acc.getAccount(email).subscribe(response => {
+      this.userarr=response
+    })
+  }
+  
+  @HostListener('window:resize')
+  onResize() {
+    this.isNavbarTogglerActive = window.innerWidth <= 992;
+  }
+
+  toggleDropdown() {
+    if (!this.isNavbarTogglerActive) {
+      this.isDropdownOpen = !this.isDropdownOpen;
+    }
+  }
+  logout(){
+    localStorage.clear()
+  }
+}
+
---
+++ dotnetapp/WebApp/Models/ReviewModel.cs
@@ -0,0 +1,22 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class ReviewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Review { get; set; }
+        public int Rating { get; set; }
+        
+        public string UserEmailId { get; set; }
+
+        
+        public int ServiceCenterId { get; set; }
+    }
+}
