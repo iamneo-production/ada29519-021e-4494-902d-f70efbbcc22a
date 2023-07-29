52c21d2bfcae54b651ed8891cb4246e62c9e0ea0
Parthipan-G <parthipang11@gmail.com>
2023-07-29 11:07:40 +0000

    Final Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomepageComponent } from './component/user/homepage/homepage.component';
+// import { DashboardComponent } from './component/user/dashboard/dashboard.component';
+// import { AppointmentComponent } from './component/user/appointment/appointment.component';
+
+
+const routes: Routes = [
+ 
+  
+  { path: 'user/homepage', component: HomepageComponent },
+  // { path: 'user/dashboard', component: DashboardComponent },
+  // { path: 'user/appointment', component: AppointmentComponent },
+
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,18 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { UserComponent } from './component/user/user.component';
+import { HomepageComponent } from './component/user/homepage/homepage.component';
+// import { DashboardComponent } from './component/user/dashboard/dashboard.component';
+// import { AppointmentComponent } from './component/user/appointment/appointment.component';
+import { ShareService } from './services/share.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+// import { CarouselComponent } from './component/carousel/carousel.component';
+
+
+
+
+
+
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    UserComponent,
+    // DashboardComponent,
+    // AppointmentComponent,
+    HomepageComponent,
+    //CarouselComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    Ng2SearchPipeModule 
   ],
-  providers: [],
+  providers: [ShareService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
